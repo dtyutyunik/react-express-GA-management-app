2bf991bc235916c6169028a44e5d4a8208cf5100
Dima <dmitriy.tyutyunik@gmail.com>
2018-12-10 15:02:57 -0500

    updated model

--- models.js
+++ models.js
@@ -38,9 +38,9 @@ const User = sequelize.define('user', {
   auth_level: Sequelize.STRING
 });
 
-Class.hasmany(Students);
+Class.hasMany(Student);
 Instructor.belongsTo(Class);
-Instructor.hasmany(Students);
+Instructor.hasMany(Student);
 
 
 module.exports = {

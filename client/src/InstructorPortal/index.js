b713cd5e0bcb5789da3a7b2629335e1ed55e4dc5
Donovan Sohr <donovansohr@ool-ad0201ba.dyn.optonline.net>
2018-12-11 17:04:22 -0500

    added instructor menu

--- client/src/InstructorPortal/index.js
+++ client/src/InstructorPortal/index.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import { Button } from 'antd/lib';
+import { Menu, Dropdown, Icon, message} from 'antd';
 
 class InstructorPortal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      current: 'user',
     }
   }
+
+//   handleClick = (e) => {
+//   console.log('click ', e);
+//   this.setState({
+//     current: e.key,
+//   });
+// }
+
   render() {
+  const onClick = ({ key }) => {
+  message.info(`Click on item ${key}`);
+};
+const SubMenu = Menu.SubMenu;
+
     return (
       <div>
         <h1>Instructor Portal</h1>
-        <Button type="primary">Primary</Button>
-        <Button>Default</Button>
-        <Button type="dashed">Dashed</Button>
-        <Button type="danger">Danger</Button>
+
+      <nav className='instructorMenu'>
+        <Menu
+          onClick={onClick}
+          selectedKeys={[this.state.current]}
+          mode="horizontal">
+          <Menu.Item key="user">
+            <Icon type="user"/>My Profile
+          </Menu.Item>
+
+        <SubMenu title={<span className="subMenu"><Icon type="team" />My Courses</span>}>
+            <Menu.Item key="team:1">Course Info</Menu.Item>
+            <Menu.Item key="team:2">Student List</Menu.Item>
+        </SubMenu>
+        </Menu>
+        </nav>
       </div>
     )
   }
 }
-
 export default InstructorPortal;

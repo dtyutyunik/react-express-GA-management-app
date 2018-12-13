605ee6839bb72a55b9b570b497f35df43ff185eb
yanHuang <yanhuang2013@gmail.com>
2018-12-13 13:52:16 -0500

    edit landing

--- client/src/LandingPage/LandingBody/index.js
+++ client/src/LandingPage/LandingBody/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import QueueAnim from 'rc-queue-anim';
+import './index.scss'
 
 export default class LandingBody extends React.Component {
   render() {
     return (
     (<QueueAnim className="landAnimate">
-      <div key="demo1" className="landing-card">First to come in</div>
-      <div key="demo2" className="landing-card">Second come in</div>
-      <div key="demo3" className="landing-card">Third coming in</div>
-      <div key="demo4" className="landing-card">Fourth coming in</div>
+      <div key="demo1" className="landing-card card-1">First Course to come in</div>
+      <div key="demo2" className="landing-card card-2">Second Course come in</div>
+      <div key="demo3" className="landing-card card-3">Third Course coming in</div>
+      <div key="demo4" className="landing-card card-4">Fourth Course coming in</div>
     </QueueAnim>)
     )
   }

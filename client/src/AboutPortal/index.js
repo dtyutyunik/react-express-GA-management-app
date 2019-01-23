e5313c3f6b4f6e50c3d2dcb1f3d83c2f92ab5d25
Dmitriy Tyutyunik <dmitriy.tyutyunik@gmail.com>
2019-01-23 14:32:15 -0500

    about me updated

--- client/src/AboutPortal/index.js
+++ client/src/AboutPortal/index.js
@@ -5,7 +5,7 @@ function AboutPortal (props){
 return(
   <div className="AboutPortal">
 
-   <p className="about-me">Situated in the heart of the bustling city, BS provides to notch <strike>meat for the grinder of todays startups</strike> promising candidates that can work in high-pressure environments for affordable costs and flexible shedules. Our high and exacting standards for <strike>industry washouts</strike> quality instructors ensure you will recieve the best training your money can buy!</p>
+   <p className="about-me">Situated in the heart of the bustling city near you, Bootcamp Startup provides top notch quality instructors. They prepare promising candidates with the skills necessary to work in high-pressure environments. We offer affordable costs and flexible shedules for many of our Technology related jobs. Some of our courses include UX, Data Science, Digital Marketing, and Web Development Immersive courses.</p>
 
   </div>
 
